d28ebca667dc1e4cbf81df0434c59183d0e163fb
Rafael Cordeiro <rafaelfelipeac@gmail.com>
2026-09-11 06:20:24 -0300

    Keep language resources in app bundle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -92,6 +92,11 @@ android {
         compose = true
         buildConfig = true
     }
+    bundle {
+        language {
+            enableSplit = false
+        }
+    }
     sourceSets {
         getByName("androidTest").assets.directories.add("schemas")
     }
